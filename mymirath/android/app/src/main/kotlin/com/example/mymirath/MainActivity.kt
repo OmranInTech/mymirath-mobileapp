1ae79b98b942d9220530b1262bceff054ccb0617
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2026-06-04 11:46:04 -0400

    project structure updated

---
+++ mymirath/android/app/src/main/kotlin/com/example/mymirath/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mymirath
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ mymirath/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MyMirath',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MyMirath Calculator'),
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to MyMirath',
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
